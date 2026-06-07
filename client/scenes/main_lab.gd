5efdf39a633e53104136533d899a38e7f022fc22
OsherBerGit <bergerosher10@gmail.com>
2026-06-08 00:08:57 +0300

    Add main lab scene and maze generation logic

---
+++ client/scenes/main_lab.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+const SOURCE_ID = 1
+const WALL_ATLAS_COORD = Vector2i(9, 4)
+const PATH_ATLAS_COORD = Vector2i(7, 12)
+
+@onready var maze_layer = $MazeRenderer
+
+func _ready() -> void:
+	var maze_engine = GodotMazeWrapper.new()
+	
+	var width = 21
+	var height = 21
+	
+	maze_engine.generate_maze(width, height, randi())
+	var data = maze_engine.get_maze_data()
+	
+	maze_layer.clear()
+	
+	for y in range(height):
+		for x in range(width):
+			var index = y * width + x
+			var cell_type = data[index]
+			var atlas_coord = PATH_ATLAS_COORD if cell_type == 1 else WALL_ATLAS_COORD
+			maze_layer.set_cell(Vector2i(x, y), SOURCE_ID, atlas_coord)
